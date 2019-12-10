4b1b6bd9c7765582d19189ccf07dcf95367948b7
mishs <mishuct26@gmail.com>
2019-12-10 13:38:35 +0200

    Creates class component having mock data for our contacts

--- contact-manager/src/App.js
+++ contact-manager/src/App.js
@@ -1,12 +1,34 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
+import Contact from "./components/Contacts";
+import Header from "./components/Header";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Main App Component</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header branding="Contact Manager" />
+        <Contact
+          name="Mish"
+          email="xyz@gmail.com phone"
+          phone="0027 000 0000"
+        />
+
+        <Contact
+          name="Chris"
+          email="jacks@gmail.com phone"
+          phone="0027 111 0000"
+        />
+      </div>
+    );
+  }
+  // what's actually happening under the hood
+
+  // return React.createElement(
+  //   "div",
+  //   { className: "App" },
+  //   React.createElement("h1", null, "This is an app")
+  // );
 }
 
 export default App;
